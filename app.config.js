786164232aa10fca7ca2c5d1017f401690d090ff
damienaltman42 <damien.altman42@gmail.com>
2025-03-15 11:22:16 +0100

    build for ios

--- app.config.js
+++ app.config.js
@@ -3,6 +3,42 @@ const withForegroundService = require("./android-manifest.plugin");
 module.exports = withForegroundService(({ config }) => {
   return {
     ...config,
+    // Définir explicitement les versions
+    version: "1.0.1",
+    ios: {
+      ...config.ios,
+      buildNumber: "1",
+      infoPlist: {
+        ...config.ios?.infoPlist,
+        LSApplicationCategoryType: "public.app-category.music",
+        NSAppTransportSecurity: {
+          NSAllowsArbitraryLoads: true,
+          NSAllowsLocalNetworking: true,
+          NSExceptionDomains: {
+            "101smoothjazz.com": {
+              NSIncludesSubdomains: true,
+              NSExceptionAllowsInsecureHTTPLoads: true
+            },
+            "cdn-profiles.tunein.com": {
+              NSIncludesSubdomains: true,
+              NSExceptionAllowsInsecureHTTPLoads: true
+            },
+            "radiofarda.com": {
+              NSIncludesSubdomains: true,
+              NSExceptionAllowsInsecureHTTPLoads: true
+            },
+            "rfi.fr": {
+              NSIncludesSubdomains: true,
+              NSExceptionAllowsInsecureHTTPLoads: true
+            },
+            "deephouselounge.com": {
+              NSIncludesSubdomains: true,
+              NSExceptionAllowsInsecureHTTPLoads: true
+            }
+          }
+        }
+      }
+    },
     // Désactiver le mode bridgeless et la nouvelle architecture
     experiments: {
       ...config.experiments,
